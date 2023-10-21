bc8f13335c1e7b92f9b607abdf0e091b000616b7
userisash <ashrafthehero423@gmail.com>
2023-10-21 21:59:19 +0300

    changed upgrades script
    made the upgrades script more dynamic

--- New Pet Clicker/Assets/Scripts/Upgrades/UpgradeManager.cs
+++ New Pet Clicker/Assets/Scripts/Upgrades/UpgradeManager.cs
@@ -1,26 +1,68 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Tier
+{
+    public string Name; // Name of the tier for easy identification
+    public TextMeshProUGUI CostText;
+    public TextMeshProUGUI IncrementText;
+    public Button PurchaseButton;
+    public int Cost;
+    public int ViewsIncrement;
+    public int FollowersIncrement;
+    public int CashIncrement;
+
+    // Method to purchase this tier's upgrade
+    public bool PurchaseUpgrade(ClickBehavior clickBehavior)
+    {
+        if (clickBehavior.GetCash() >= Cost)
+        {
+            clickBehavior.AddCash(-Cost);
+            clickBehavior.AddToClickValues(ViewsIncrement, FollowersIncrement, CashIncrement);
+            Cost *= 2;
+            UpdateTexts();
+            return true;
+        }
+        return false;
+    }
+
+    // Update the UI elements for this tier
+    public void UpdateTexts()
+    {
+        CostText.text = "$: " + Cost.ToString();
+        IncrementText.text = GetIncrementDescription();
+    }
+
+    private string GetIncrementDescription()
+    {
+        string desc = "";
+        if (ViewsIncrement > 0) desc += $"Views +{ViewsIncrement}, ";
+        if (FollowersIncrement > 0) desc += $"Followers +{FollowersIncrement}, ";
+        if (CashIncrement > 0) desc += $"Cash +{CashIncrement}, ";
+        return desc.TrimEnd(',', ' ');
+    }
+}
 
 public class UpgradeManager : MonoBehaviour
 {
     public ClickBehavior ClickBehavior;
     public GameObject popupMenu;
+    public List<Tier> tiers;
 
-    public TextMeshProUGUI tierOneCostText;
-    public TextMeshProUGUI tierTwoCostText;
+    private void Start()
+    {
+        // Assign button callbacks
+        foreach (var tier in tiers)
+        {
+            Button btn = tier.PurchaseButton;
+            int tierIndex = tiers.IndexOf(tier); // Get the index of the current tier
+            btn.onClick.AddListener(() => tiers[tierIndex].PurchaseUpgrade(ClickBehavior));
+        }
+    }
 
-
-    public TextMeshProUGUI tierOneIncrementText;
-    public TextMeshProUGUI tierTwoIncrementText;
-
-    public Button tierOneButton;
-    public Button tierTwoButton;
-    //... other tier buttons...
-
-    public int tierOneCost = 10;
-    public int tierTwoCost = 20;
-    //... other tier costs...
 
     private void Update()
     {
@@ -30,58 +72,22 @@ public class UpgradeManager : MonoBehaviour
 
     private void UpdateButtonInteractability()
     {
-        tierOneButton.interactable = ClickBehavior.GetCash() >= tierOneCost;
-        tierTwoButton.interactable = ClickBehavior.GetCash() >= tierTwoCost;
-        //... Similarly for other tiers...
-    }
-
-
-
-    public void PurchaseTierOneUpgrade()
-    {
-        if (ClickBehavior.GetCash() >= tierOneCost)
+        foreach (var tier in tiers)
         {
-            ClickBehavior.AddCash(-tierOneCost);
-            ClickBehavior.AddToClickValues(2, 0, 0);
-            tierOneCost *= 2;
-            UpdateText(tierOneCostText, tierOneCost);
+            tier.PurchaseButton.interactable = ClickBehavior.GetCash() >= tier.Cost;
         }
     }
-
-
-
-    public void PurchaseTierTwoUpgrade()
-    {
-        if (ClickBehavior.GetCash() >= tierTwoCost)
-        {
-            ClickBehavior.AddCash(-tierTwoCost);
-            ClickBehavior.AddToClickValues(2, 2, 0);
-            tierTwoCost *= 2;
-            UpdateText(tierTwoCostText, tierTwoCost);
-        }
-    }
-
 
     private void UpdateAllTexts()
     {
-        UpdateText(tierOneCostText, tierOneCost);
-        UpdateText(tierTwoCostText, tierTwoCost);
-       
-        tierOneIncrementText.text = "Views +2";
-        tierTwoIncrementText.text = "Views +2, Followers +1";
-       
+        foreach (var tier in tiers)
+        {
+            tier.UpdateTexts();
+        }
     }
-
-    private void UpdateText(TextMeshProUGUI targetText, int value)
-    {
-        targetText.text = "$: " + value.ToString();
-    }
-
-    //... repeat for other tiers...
 
     public void ToggleMenu()
     {
-        Debug.Log("ToggleMenu method called.");
         popupMenu.SetActive(!popupMenu.activeSelf);
     }
 }
