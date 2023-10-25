1ea37efc96a039bf3cc7ef51d07a04335509992d
userisash <ashrafthehero423@gmail.com>
2023-10-25 21:59:28 +0300

    Bug Fixes
    1- player can no longer interact with the slider on the business fill bar
    
    2- panels no longer open together

---
+++ New Pet Clicker/Assets/Scripts/Panels/PopupPanel.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupPanel : MonoBehaviour
+{
+    //public Button openButton;
+
+    //private void Start()
+    //{
+    //    openButton.onClick.AddListener(TryOpenPanel);
+    //}
+
+    //private void TryOpenPanel()
+    //{
+    //    Debug.Log("TryOpenPanel called for: " + gameObject.name);
+    //    PanelManager.instance.RequestToOpenPanel(gameObject);
+    //}
+
+    //// Assuming you have a close button or a method to close the panel
+    //public void CloseThisPanel()
+    //{
+    //    PanelManager.instance.ClosePanel(gameObject);
+    //}
+}
