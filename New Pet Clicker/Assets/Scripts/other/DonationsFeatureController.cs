ecd59e3392a3333c2c1218f098dcbac53416d6e9
userisash <ashrafthehero423@gmail.com>
2023-11-06 17:37:00 +0200

    added donations system

---
+++ New Pet Clicker/Assets/Scripts/other/DonationsFeatureController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DonationsFeatureController : MonoBehaviour
+{
+    public ClickBehavior clickBehavior;
+    public TextMeshProUGUI donationsFeatureText;
+
+    private bool isDonationsFeatureUnlocked = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDonationsFeatureUnlocked && clickBehavior.followers >= 10000)
+        {
+            UnlockDonationsFeature();
+        }
+    }
+
+    private void UnlockDonationsFeature()
+    {
+        isDonationsFeatureUnlocked = true;
+        donationsFeatureText.text = "Donations Unlocked!";
+    }
+
+    public void TryGenerateDonation()
+    {
+        if (isDonationsFeatureUnlocked && clickBehavior.followers % 10 == 0)
+        {
+            // 20% chance to generate a donation
+            if (Random.Range(0, 5) == 0) // There's 1 in 5 chance for this to be true, which corresponds to 20%
+            {
+                int donationAmount = Random.Range(5, 11); // Generates a random donation between 5 and 10
+                clickBehavior.AddCash(donationAmount);
+            }
+        }
+    }
+}
