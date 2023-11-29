ed382ff009506cb231c0181a4b3e463f364a7406
KentNotdoit <leaveplox@gmail.com>
2023-11-29 22:01:29 +0200

    more UI enhancment and animation

--- New Pet Clicker/Assets/Scripts/Panels/PanelManager.cs
+++ New Pet Clicker/Assets/Scripts/Panels/PanelManager.cs
@@ -7,62 +7,74 @@ public class PanelManager : MonoBehaviour
     private List<GameObject> panels = new List<GameObject>();
 
     public float moveAmount = 500f; // Amount to move the panel
-    public float speed = 1f; // Speed of the animation
+    public float speed = 1f; // Speed of the movement animation
+    public float fadeSpeed = 0.5f; // Speed of the fade animation, faster than movement
 
     private GameObject activePanel = null;
 
     public void TogglePanel(GameObject panelToToggle)
     {
-        // If there's an active panel and it's not the one we're trying to toggle, hide it
         if (activePanel != null && activePanel != panelToToggle)
         {
-            GameObject panelToDeactivate = activePanel; // Store the current active panel for deactivation
-            LeanTween.moveLocalX(activePanel, -moveAmount, speed)
-                    .setEaseOutQuad()
-                    .setOnComplete(() => {
-                        panelToDeactivate.SetActive(false); // Deactivate the previously active panel
-                });
-            activePanel = null; // Reset the activePanel as it's now being hidden
+            StartHidingPanel(activePanel);
+            activePanel = null;
         }
 
-        // If the panel to toggle is not already active, show it
         if (panelToToggle != null && !panelToToggle.activeSelf)
         {
-            panelToToggle.SetActive(true);
-            panelToToggle.transform.localPosition = new Vector3(-moveAmount, panelToToggle.transform.localPosition.y, panelToToggle.transform.localPosition.z);
-            LeanTween.moveLocalX(panelToToggle, 0, speed).setEaseOutQuad();
-            activePanel = panelToToggle; // Set the new panel as active
+            ShowPanel(panelToToggle);
+            activePanel = panelToToggle;
         }
-        // If the panel to toggle is already active, hide it
         else if (panelToToggle != null && panelToToggle.activeSelf)
         {
-            LeanTween.moveLocalX(panelToToggle, -moveAmount, speed)
-                    .setEaseOutQuad()
-                    .setOnComplete(() => {
-                        panelToToggle.SetActive(false); // Deactivate the panel being hidden
-                    if (activePanel == panelToToggle)
-                        {
-                            activePanel = null; // Reset the activePanel as it's now hidden
-                    }
-                    });
+            StartHidingPanel(panelToToggle);
+            if (activePanel == panelToToggle)
+            {
+                activePanel = null;
+            }
         }
     }
 
-
-
-
-
-    public void InitializePanels()
+    private void ShowPanel(GameObject panel)
     {
-        foreach (var panel in panels)
-        {
-            panel.SetActive(false);
-            panel.transform.localPosition = new Vector3(-moveAmount, panel.transform.localPosition.y, panel.transform.localPosition.z);
-        }
+        panel.SetActive(true);
+        CanvasGroup canvasGroup = panel.GetComponent<CanvasGroup>();
+        if (canvasGroup == null) canvasGroup = panel.AddComponent<CanvasGroup>();
+
+        canvasGroup.alpha = 0;
+        LeanTween.alphaCanvas(canvasGroup, 1, fadeSpeed); // Use fadeSpeed for fade-in
+        panel.transform.localPosition = new Vector3(-moveAmount, panel.transform.localPosition.y, panel.transform.localPosition.z);
+        LeanTween.moveLocalX(panel, 0, speed).setEaseOutQuad();
+    }
+
+    private void StartHidingPanel(GameObject panel)
+    {
+        CanvasGroup canvasGroup = panel.GetComponent<CanvasGroup>();
+        if (canvasGroup == null) canvasGroup = panel.AddComponent<CanvasGroup>();
+
+        LeanTween.alphaCanvas(canvasGroup, 0, fadeSpeed) // Use fadeSpeed for fade-out
+            .setOnComplete(() => {
+                panel.SetActive(false);
+            });
+
+        LeanTween.moveLocalX(panel, -moveAmount, speed).setEaseOutQuad();
     }
 
     void Start()
     {
         InitializePanels();
+    }
+
+    private void InitializePanels()
+    {
+        foreach (var panel in panels)
+        {
+            panel.SetActive(false);
+            if (panel.GetComponent<CanvasGroup>() == null)
+            {
+                panel.AddComponent<CanvasGroup>();
+            }
+            panel.transform.localPosition = new Vector3(-moveAmount, panel.transform.localPosition.y, panel.transform.localPosition.z);
+        }
     }
 }
